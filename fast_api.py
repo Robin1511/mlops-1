5bbc9922357319881eb735e0891b18479cb92f8d
Dane De Bastos <danedebastos@MacBook-Pro-de-Dane.local>
2025-09-08 14:12:18 +0200

    level 0 2.0

--- fast_api.py
+++ fast_api.py
@@ -11,16 +11,15 @@ model = joblib.load("regression.joblib")
 def read_root():
     return {"message": "Bonjour, lancez le serveur avec : 'uvicorn fast_api:app --reload'"}
 
-
 class HouseFeatures(BaseModel):
     size: int
     nb_rooms: int
     garden: bool
 
 @app.post("/predict")
-def predict(features: HouseFeatures):
-    garden_int = 1 if features.garden else 0
-    y_pred = model.predict([[features.size, features.nb_rooms, garden_int]])
+def predict(size: int, nb_rooms: int, garden: bool):
+    garden_int = 1 if garden else 0
+    y_pred = model.predict([[size, nb_rooms, garden_int]])
     return {"y_pred": float(y_pred[0])}
 
 if __name__ == "__main__":
