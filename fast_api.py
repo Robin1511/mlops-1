b7655b00168b0bad1b295c641d4f75345527e1a1
Dane De Bastos <danedebastos@MacBook-Pro-de-Dane.local>
2025-09-08 15:05:51 +0200

    level 1

--- fast_api.py
+++ fast_api.py
@@ -18,4 +18,4 @@ def predict(size: int, nb_rooms: int, garden: bool):
     return {"y_pred": float(y_pred[0])}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=6670)
