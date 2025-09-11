16de8f442512f414d2bff9a0ebf2a85b1dde6759
Dane De Bastos <danedebastos@MacBook-Pro-de-Dane.local>
2025-09-11 14:37:36 +0200

    level4

--- fast_api.py
+++ fast_api.py
@@ -2,10 +2,15 @@ import fastapi
 import uvicorn
 from pydantic import BaseModel
 import joblib
+from transformers import pipeline
 
 app = fastapi.FastAPI()
 
 model = joblib.load("regression.joblib")
+sentiment_model = pipeline("sentiment-analysis", model="cardiffnlp/twitter-roberta-base-sentiment-latest")
+
+class TextInput(BaseModel):
+    text: str
 
 @app.get("/")
 def read_root():
@@ -16,6 +21,11 @@ def predict(size: int, nb_rooms: int, garden: bool):
     garden_int = 1 if garden else 0
     y_pred = model.predict([[size, nb_rooms, garden_int]])
     return {"y_pred": float(y_pred[0])}
+
+@app.post("/sentiment")
+def predict_sentiment(input_data: TextInput):
+    result = sentiment_model(input_data.text)
+    return {"sentiment": result[0]["label"], "confidence": result[0]["score"]}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=6670)
